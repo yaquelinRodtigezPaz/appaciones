3b21e6c8a065595326da0308e32332a3400aeac4
yaquelinRodtigezPaz <yaquelin.dam@gmail.com>
2016-05-18 09:03:24 -0500

    3subida

---
+++ js/acciones6.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+    document .addEventListener("deviceready",function(){
+		
+		$('#beep').tap(function (){
+			navigator .notification.beep (1);
+		});//tap beep
+		
+		$('#vibrar').tap(function (){
+			navigator .notification.vibrate (100);
+		});//tap vibrar
+		
+	},false);//deviceready
+});//ready
